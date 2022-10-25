de9be8ad8bbde112e33218a1b1e81fc814014bd4
Michele922 <Michelesijbring@hotmail.com>
2022-10-25 17:58:49 +0200

    test getYieldForCrop succeed

--- functions.js
+++ functions.js
@@ -2,6 +2,12 @@ const getYieldForPlant = (plant) => {
   return plant.yield;
 };
 
+const getYieldForCrop = (input) => {
+  const { crop, numCrops } = input;
+  return getYieldForPlant(crop) * numCrops;
+};
+
 module.exports = {
   getYieldForPlant,
+  getYieldForCrop,
 };
